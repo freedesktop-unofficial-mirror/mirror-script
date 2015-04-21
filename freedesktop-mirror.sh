4b9afffde1186b9642f380d5c7539643aae57487
Jordan Klassen <jordan@klassen.me.uk>
2015-04-20 23:55:41 -0700

    update script
    this is the one used to perform the mirror

--- freedesktop-mirror.sh
+++ freedesktop-mirror.sh
@@ -845,11 +845,15 @@ repos=(
   realmd/realmd
 )
 
-cd ..;
+cd "$(dirname "$0")/..";
 
 for repo in "${repos[@]}" ; do
   # replace / with __ in repository name
   hubrepo="${repo//\//__}"
-  git clone --mirror git://anongit.freedesktop.org/"$repo" "$hubrepo"
-  
+  git clone --mirror git://anongit.freedesktop.org/"$repo" "$hubrepo" &&
+  (
+    cd $hubrepo
+    hub create -d "(mirror)" -h "http://cgit.freedesktop.org/$repo" freedesktop-unofficial-mirror/"$hubrepo" &&
+    git push --mirror git@github.com:freedesktop-unofficial-mirror/$hubrepo
+  )
 done
